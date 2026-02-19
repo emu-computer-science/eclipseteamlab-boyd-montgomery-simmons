81a35b8f984bd3477fcf4f2bb272ed054a8f9583
AlexGlennBoyd <aboyd34@emich.edu>
2026-02-18 20:42:05 -0500

    This is step 17; I added a new method to Hungry.java

--- src/Glad.java
+++ src/Glad.java
@@ -4,4 +4,8 @@ public class Glad {
 	public static void glad() {
 		System.out.println("We are feeling glad.");
 	}
+
+	public static void hungry2() {
+		System.out.println("We are still hungry!");
+	}
 }
